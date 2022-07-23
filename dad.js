0e7af3957c678ad5ee3a58abc7e2607ac483481d
EZow25 <evanzhao90@gmail.com>
2022-07-23 01:54:03 -0400

    Made Positions of Divs Fixed + Added Dawg Button

--- dad.js
+++ dad.js
@@ -1,21 +1,44 @@
 document.addEventListener('click', event => {
-    if (event.target.id === 'button') {
-      async function fetchJoke() {
-        try {
-          const response = await fetch("https://icanhazdadjoke.com", {
-            headers: {
-              "Accept" : "application/json"
-            }
-          })
-          const joke = await response.json()
-          console.log(joke)
-          var p = document.getElementById("funny")
-          p.innerText = joke['joke']
-        } catch(error) {
-          console.error(error)
-        }
-      }  
+  if (event.target.id === 'button') {
+    async function fetchJoke() {
+      try {
+        const response = await fetch("https://icanhazdadjoke.com", {
+          headers: {
+            "Accept" : "application/json"
+          }
+        })
+        const joke = await response.json()
+        console.log(joke)
+        var p = document.getElementById("funny")
+        p.innerText = joke['joke']
+      } catch(error) {
+        console.error(error)
+      }
+    }  
       
-      fetchJoke()
+    fetchJoke()
+  }
+  
+  if (event.target.id === 'dog') {
+    fetchGivenJoke('dog')
   }
 })
+
+async function fetchGivenJoke(search) {
+  try {
+    const page = Math.floor(Math.random() * 12) + 1
+    console.log(page) 
+    const response = await fetch(`https://icanhazdadjoke.com/search?page=${page}&limit=1&term=${search}`, {
+      headers: {
+        "Accept" : "application/json"
+      }
+    })
+    const joke = await response.json()
+    console.log(joke)
+    var p = document.getElementById("funny")
+    p.innerText = joke['results'][0]['joke']
+  } catch(error) {
+    console.error(error)
+  }
+}
+
